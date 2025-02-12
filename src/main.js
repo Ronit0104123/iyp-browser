f1c18c72a239434bbc8671f2618cca5323184223
Dimitrios Giakatos <dimitrios@iij.ad.jp>
2025-02-12 12:47:18 +0900

    add quasar

--- src/main.js
+++ src/main.js
@@ -3,9 +3,18 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { Quasar } from 'quasar'
+
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+
+import 'quasar/dist/quasar.css'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(Quasar, {
+    plugins: {}
+})
 
 app.mount('#app')
