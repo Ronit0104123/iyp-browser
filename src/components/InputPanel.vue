ae0e72bccfbfd4a5ca4295e399ded6b81bbec3fc
Ronit0104123 <ronitj0104@gmail.com>
2025-04-05 05:04:36 +0530

    features added

--- src/components/InputPanel.vue
+++ src/components/InputPanel.vue
@@ -1,11 +1,13 @@
 <script setup>
-import { ref, onMounted, watch } from "vue";
+import { ref, onMounted, watch, inject } from "vue";
 import * as monaco from "monaco-editor";
 import schema from "@/assets/neo4j-schema.json";
 import { autocomplete } from "@neo4j-cypher/language-support";
 import Feedback from "./Feedback.vue";
+import interact from "interactjs";
 
 const emits = defineEmits(["run", "clear"]);
+const Neo4jApi = inject("Neo4jApi");
 
 const props = defineProps([
   "cypherInput",
@@ -14,6 +16,7 @@ const props = defineProps([
   "serveInOutput",
 ]);
 
+const inputPanel = ref();
 const code = ref();
 const tab = ref("");
 let cypher = "";
@@ -80,8 +83,25 @@ watch(
   },
 );
 
-onMounted(() => {
+onMounted(async () => {
   tab.value = "cypher";
+  interact(inputPanel.value)
+    .origin("self")
+    .resizable({
+      edges: { bottom: true },
+      inertia: true,
+      listeners: {
+        move(event) {
+          inputPanel.value.style.height = `${event.rect.height}px`;
+        },
+      },
+      modifiers: [
+        interact.modifiers.restrictSize({
+          min: { height: 100 },
+          max: { height: 300  },
+        }),
+      ],
+    });
   editor = monaco.editor.create(code.value, {
     value: "",
     language: "cypher",
@@ -100,29 +120,80 @@ onMounted(() => {
     extensions: [".cypher"],
     aliases: ["Cypher", "cypher"],
   });
+
+  //console.log("Schema", schema)
+  // const res = await Neo4jApi.run("CALL schema.visualization()");
+  // const res2 = await Neo4jApi.run('MATCH (n)-[r]-(m) RETURN DISTINCT labels(n), type(r), labels(m)');
+  // console.log("schema2", res)
+  // console.log("Cypher-approach ", res2) 
+
   monaco.languages.registerCompletionItemProvider("cypher", {
-    provideCompletionItems: (model, position) => {
+    triggerCharacters: [' ', ':', '(', ')', '{', '}', ...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'],
+    provideCompletionItems: async (model, position) => {
       const textUtilPosition = model.getValueInRange({
         startLineNumber: 1,
         startColumn: 1,
         endLineNumber: position.lineNumber,
         endColumn: position.column,
       });
+
+    let matchNode = textUtilPosition.match(/:\s*([A-Za-z0-9_]+)/);
+    let matchRel = textUtilPosition.match(/\[\s*:\s*([A-Za-z0-9_]+)/);
+    let dynamicSuggestions = [];
+
+    try {
+      if (matchNode && matchNode[1].length > 1) {
+        const nodeLabel = matchNode[1];
+        const res = await Neo4jApi.run(
+          `MATCH (n:\`${nodeLabel}\`)-[r]-(m) RETURN DISTINCT type(r) AS rel, labels(m) AS node`
+        );
+        // console.log("matchNode", matchNode)
+        // console.log("res", res)
+        res.table.rows.forEach((item) => {
+          dynamicSuggestions.push({
+            label: `${item.rel} -> ${item.node}`,
+            kind: monaco.languages.CompletionItemKind.Keyword,
+            insertText: `${item.rel} -> (${item.node})`,
+          });
+        });
+      } else if (matchRel) {
+        const relType = matchRel[1];
+        const res = await Neo4jApi.run(
+          `MATCH (n)-[r:\`${relType}\`]-(m) RETURN DISTINCT labels(n) AS source, labels(m) AS target`
+        );
+        res.forEach((item) => {
+          dynamicSuggestions.push({
+            label: `${item.source} -[${relType}]- ${item.target}`,
+            kind: monaco.languages.CompletionItemKind.Keyword,
+            insertText: `(${item.source})-[:${relType}]-(${item.target})`,
+          });
+        });
+      }
+    } catch (err) {
+      // console.error("AutoComplete Query Error: ", err);
+    }
+
+
       const completionItems = autocomplete(textUtilPosition, schema);
-      return {
-        suggestions: completionItems.map((item) => ({
-          label: item.label,
-          kind: item.kind,
-          insertText: item.label,
-          range: item.range,
-        })),
-      };
+      const staticSuggestions = completionItems.map((item) => ({
+      label: item.label,
+      kind: item.kind,
+      insertText: item.label,
+      range: item.range,
+      }));
+
+    return {
+      suggestions: [...staticSuggestions, ...dynamicSuggestions],
+    };
     },
   });
   editor.addAction({
     id: "run",
     label: "Run",
-    keybindings: [monaco.KeyMod.Shift | monaco.KeyCode.Enter],
+    keybindings: [
+      monaco.KeyMod.Shift | monaco.KeyCode.Enter,
+      monaco.KeyMod.CtrlCmd | monaco.KeyCode.Enter
+    ],
     run: () => {
       runQuery();
     },
@@ -131,14 +202,14 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="input-container row">
+  <div class="input-container row" ref="inputPanel">
     <q-tabs v-model="tab" class="input-language-switcher" vertical dense>
       <q-tab name="cypher" label="Cypher" />
       <q-tab name="text" label="Text" disable>
         <q-badge>Coming Soon</q-badge>
       </q-tab>
     </q-tabs>
-    <div ref="code" class="code col q-mr-md"></div>
+    <div ref="code" class="code col q-mr-md"></div>  
     <div class="col-auto q-mr-md">
       <div class="row q-mb-sm">
         <q-btn
