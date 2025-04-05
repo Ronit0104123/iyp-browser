2e04c78c96806e924781017f3331bc868717a373
Ronit0104123 <ronitj0104@gmail.com>
2025-04-05 05:32:58 +0530

    Updates

--- assets/index-CuCBxNvP.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/index.umd-CUtyY9BN.js","assets/index-CFH4OFMu.js","assets/index-CfTM5phW.css"])))=>i.map(i=>d[i]);
-import{_ as n,a as o,v as a}from"./index-CFH4OFMu.js";function _(i){return n(this,void 0,void 0,function(){var t;return o(this,function(e){switch(e.label){case 0:return[4,a(()=>import("./index.umd-CUtyY9BN.js").then(r=>r.i),__vite__mapDeps([0,1,2]))];case 1:return t=e.sent(),i._plugins=t,[2]}})})}export{_ as loadLegacyVideoPlugins};
---
+++ assets/index-DDMLHRds.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/index.umd-DJMgyvLF.js","assets/index-DAqz5ibM.js","assets/index-CddRqK2o.css"])))=>i.map(i=>d[i]);
+import{_ as n,a as o,v as a}from"./index-DAqz5ibM.js";function _(i){return n(this,void 0,void 0,function(){var t;return o(this,function(e){switch(e.label){case 0:return[4,a(()=>import("./index.umd-DJMgyvLF.js").then(r=>r.i),__vite__mapDeps([0,1,2]))];case 1:return t=e.sent(),i._plugins=t,[2]}})})}export{_ as loadLegacyVideoPlugins};
