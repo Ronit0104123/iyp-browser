024623a2b92952016c740ce179f3aed26c10877d
Dimitrios Giakatos <dimitrios@iij.ad.jp>
2025-05-30 17:24:06 +0900

    change IYP URL

--- src/plugins/Neo4jApi.js
+++ src/plugins/Neo4jApi.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import randomColor from "randomcolor";
 
 /// Base url for neo4j api
-// const NEO4J_API_BASE = "https://iyp.iijlab.net/iyp/db/neo4j/query/v2";
-const NEO4J_API_BASE = "https://iyp-dev.ihr.live/db/neo4j/query/v2";
+const NEO4J_API_BASE = "https://iyp.iijlab.net/iyp/db/neo4j/query/v2";
+
 /// Default timeout before api call are considered failed
 const DEFAULT_TIMEOUT = 180000;
 
